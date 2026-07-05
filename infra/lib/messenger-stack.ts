ae8e41642a1bd5030986cc636b03b8f1dfaed38e
Rafa <rafag.1858@hotmail.es>
2026-07-06 00:24:23 +0200

    Create the Nodegroup. I'm going for EC2 instead of Fargate because I need EBS to persist the DB. In the future we could consider Fargate for stateless workloads.

--- infra/lib/messenger-stack.ts
+++ infra/lib/messenger-stack.ts
@@ -1,7 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
-import { Cluster, KubernetesVersion } from 'aws-cdk-lib/aws-eks';
+import { AlbControllerVersion, CapacityType, Cluster, KubernetesVersion } from 'aws-cdk-lib/aws-eks';
 import { Construct } from 'constructs';
 import { KubectlV35Layer } from '@aws-cdk/lambda-layer-kubectl-v35';
+import { FargateCluster } from 'aws-cdk-lib/aws-eks-v2';
+import { InstanceType } from 'aws-cdk-lib/aws-ec2';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -9,16 +11,24 @@ export class MessengerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
     const cluster = new Cluster(this, 'MessengerCluster', {
       version: KubernetesVersion.V1_32,
       kubectlLayer: new KubectlV35Layer(this, 'kubectl'),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      // We need the ALB controller to support the Ingress.
+      albController: {
+        version: AlbControllerVersion.V3_2_2,
+      },
+      defaultCapacity: 0, // The capacity is defined later.
+    });
+
+    cluster.addNodegroupCapacity('MessengerNodegroup', {
+      instanceTypes: [new InstanceType('m5.large')],
+      minSize: 2,
+      maxSize: 4,
+      diskSize: 5,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      capacityType: CapacityType.ON_DEMAND, // [IMPROVE] Consider using SPOT for stateless services.
     });
   }
 }
